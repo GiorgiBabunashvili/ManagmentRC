be787409a465e1c569ae448f6627710ad9aae888
shanME <giorgie103188@gmail.com>
2024-07-30 18:33:02 +0400

    styling sidebar

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,5 +1,12 @@
 const ProjectsSidebar = () => {
-  return <aside></aside>;
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+    </aside>
+  );
 };
 
 export default ProjectsSidebar;
