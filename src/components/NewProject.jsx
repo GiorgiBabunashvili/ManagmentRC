7eadd287518b3e7c1b3b3fcb404f7ed400ed312e
shanME <giorgie103188@gmail.com>
2024-07-30 21:31:56 +0400

    styling modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 const NewProject = ({ onAddProject }) => {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -16,6 +18,7 @@ const NewProject = ({ onAddProject }) => {
       enteredDescription.trim() === "" ||
       enteredDueDate.trim() === ""
     ) {
+      modal.current.open();
       return;
     }
 
@@ -27,28 +30,35 @@ const NewProject = ({ onAddProject }) => {
   }
 
   return (
-    <div className="w-[35rem]  mt-16 ">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-400 hover:text-stone-950">
-            Cancle
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" isTextarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} btnCaption="Close">
+        <h1>Invalid Input</h1>
+        <p>Oops... lookc lika you forgot to enter a value</p>
+        <p>please make sure you provide a valid value for every input field</p>
+      </Modal>
+      <div className="w-[35rem]  mt-16 ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-400 hover:text-stone-950">
+              Cancle
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" isTextarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
